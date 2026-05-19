480e40c310d84a24d401cb1a302b7f9944e1cce5
merabtenelynda <lynda.merabtene2001@gmail.com>
2026-05-19 13:46:21 +0200

    fix images import logo.png

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import logo from './assets/logo.png'
+import logo from '../assets/logo.png'
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false)
